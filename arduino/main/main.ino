e0e03365b18d30c4d2ffe2356d85a3f68e463769
Andrii Chebanenko <andriy040105@gmail.com>
2024-11-23 17:02:28 +0200

    Add track_visitors task

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,17 +1,25 @@
+/* Sensor 1 - outer side of entrance
+   Sensor 2 - inner sensor of the entrance
+*/
+
 #define LED_PIN 2
 #define SENSOR_1_PIN 5
 #define SENSOR_2_PIN 8
+#define TRACK_VISITORS_PRIORITY 1
 
 typedef enum {COUNTER, ALARM} workmode;
 
 int enters_counter;
 int exits_counter;
-int n = 0;
+TaskHandle_t track_visitors_handle;
+
+void track_visitors(void* p_Parameters);
 
 void setup() {
   pinMode(SENSOR_1_PIN, INPUT);
   pinMode(SENSOR_2_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
+  xTaskCreate(track_visitors, "Track visitors", 256, NULL, TRACK_VISITORS_PRIORITY, &track_visitors_handle);
 }
 
 void blink_led(void) {
@@ -30,5 +38,24 @@ void loop() {
       break;
     case ALARM:
       break;
+  }
+}
+
+void track_visitors(void* p_Parameters) {
+  (void)p_Parameters;
+  uint8_t sensor_1_read = LOW;
+  uint8_t sensor_2_read = LOW;
+
+  for (;;) {
+    sensor_1_read = digitalRead(SENSOR_1_PIN);
+    sensor_2_read = digitalRead(SENSOR_2_PIN);
+    if (sensor_1_read == HIGH && sensor_2_read == LOW) {
+      enters_counter++;
+      delay(500);
+    }
+    if (sensor_1_read == LOW && sensor_2_read == HIGH) {
+      exits_counter++;
+      delay(500);
+    }
   }
 }
