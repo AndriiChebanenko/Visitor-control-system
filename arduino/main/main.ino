c4b32ebb920aee97e6781c871c684e463e0a4105
Andrii Chebanenko <andriy040105@gmail.com>
2024-11-25 13:10:47 +0200

    Add test_sensor

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -12,20 +12,19 @@ typedef enum {COUNTER, ALARM} workmode;
 int enters_counter;
 int exits_counter;
 TaskHandle_t track_visitors_handle;
+TaskHandle_t test_sensor_handle;
 
+void blink_led(void);
 void track_visitors(void* p_Parameters);
+void test_sensor(void* p_Parameters);
 
 void setup() {
   pinMode(SENSOR_1_PIN, INPUT);
   pinMode(SENSOR_2_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
   xTaskCreate(track_visitors, "Track visitors", 256, NULL, TRACK_VISITORS_PRIORITY, &track_visitors_handle);
-}
-
-void blink_led(void) {
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  digitalWrite(LED_PIN, LOW);    
+  int sensor_num = 1;
+  xTaskCreate(test_sensor, "Test sensor", 256, (void*)&sensor_num, 1, &test_sensor_handle);
 }
 
 // the loop function runs over and over again forever
@@ -39,6 +38,15 @@ void loop() {
     case ALARM:
       break;
   }
+  delay(1000);
+  blink_led();
+}
+
+void blink_led(void) {
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  delay(500); 
 }
 
 void track_visitors(void* p_Parameters) {
@@ -56,6 +64,34 @@ void track_visitors(void* p_Parameters) {
     if (sensor_1_read == LOW && sensor_2_read == HIGH) {
       exits_counter++;
       delay(500);
+    }
+  }
+}
+
+void test_sensor(void* p_Parameters)
+{
+  int sensor_num = *((int*)p_Parameters);
+  int sensor_pin;
+  switch(sensor_num)
+  {
+    case 1:
+      sensor_pin = SENSOR_1_PIN;
+      break;
+    case 2:
+      sensor_pin = SENSOR_2_PIN;
+      break;
+    default:
+      return;
+  }
+  int val;
+  for (;;)
+  {
+    val = digitalRead(sensor_pin);
+    if (val == HIGH)
+    {
+      digitalWrite(LED_PIN, HIGH);
+      delay(500);
+      digitalWrite(LED_PIN, LOW);
     }
   }
 }
